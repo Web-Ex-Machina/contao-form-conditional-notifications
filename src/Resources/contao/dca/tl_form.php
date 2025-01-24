1494d25435da3d617bb2af134e808085602e3d7d
LupusVII <jln.thirion@gmail.com>
2025-01-24 17:11:30 +0100

    Fix icon path

--- src/Resources/contao/dca/tl_form.php
+++ src/Resources/contao/dca/tl_form.php
@@ -10,6 +10,6 @@ $GLOBALS['TL_DCA']['tl_form']['config']['ctable'][] = 'tl_wem_form_conditional_n
  */
 $GLOBALS['TL_DCA']['tl_form']['list']['operations']['wem_conditional_notifications'] = array
 (
-	'href'                => 'table=tl_wem_form_conditional_notification',
-	'icon'                => 'system/modules/wem-contao-form-conditional-notifications/assets/backend/icon_notifications_16.gif'
+	'href' => 'table=tl_wem_form_conditional_notification',
+	'icon' => 'bundles/wemformconditionalnotifications/backend/icon_notifications_16.gif'
 );
